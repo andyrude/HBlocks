deed3b75883af88e2ef2d8524b0dc7bfaa42cd74
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-02-27 23:50:34 +0530

    Jump Game II

--- src/JumpGame/JumpGameII.java
+++ src/JumpGame/JumpGameII.java
@@ -9,18 +9,19 @@ public class JumpGameII {
         String[] str = sc.nextLine().split(" ");
         int[] arr = new int[str.length];
         for ( int i = 0; i < str.length; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(str[i]);
         }
         System.out.println(getMinJumps( arr));
     }
 
-    private static int getMinJumps(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[arr.length];
-        Arrays.fill( dp, Integer.MAX_VALUE);
-        for ( int i = arr.length - 1; i >= 0; i++){
+    private static int getMinJumps(int[] intArr) {
+        int n = intArr.length;
+        int[] dp = new int[intArr.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n - 1] = 0;
+        for( int i = n - 2; i >= 0; i--){
             int min = Integer.MAX_VALUE;
-            for ( int j = i + 1; j <=  Math.min( n - 1, i + arr[i]); j++){
+            for ( int j = i + 1; j <= Math.min( n - 1, i + intArr[i]); j++){
                 min = Math.min( min, dp[j]);
             }
             if( min != Integer.MAX_VALUE){
