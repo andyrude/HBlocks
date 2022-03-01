de61c61b4c861fa0adcad785c4fcaeb463016aa4
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-01 20:29:28 +0530

    Two Sum

--- src/leetcode/arrays/TwoSum.java
+++ src/leetcode/arrays/TwoSum.java
@@ -1,4 +1,27 @@
 package leetcode.arrays;
 
+import java.util.Scanner;
+
 public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int[] arr = new int[str.length];
+        for ( int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt( str[i]);
+        }
+        int target = sc.nextInt();
+        sol( arr, target);
+    }
+
+    private static void sol(int[] arr, int target) {
+        for ( int i = 0; i < arr.length - 1; i++){
+            for ( int j = i + 1; j < arr.length; j++){
+                if ( arr[i] + arr[j] == target){
+                    System.out.println("[" + i + "," + j + "]");
+                    break;
+                }
+            }
+        }
+    }
 }
