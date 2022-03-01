71c7ebb2b17b9e15ec5a52fd2d0cccbebf496bae
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-01 11:53:19 +0530

    UniquePaths

---
+++ src/leetcode/UniquePathsIIDP.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class UniquePathsIIDP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(getMaxPaths(n, m));
+    }
+
+    private static int getMaxPaths(int n, int m) {
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
