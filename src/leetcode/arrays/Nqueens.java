4dc4b4a1de4b00df964dc4864827aa8f942c800d
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-02 12:12:51 +0530

    N Queens

--- src/leetcode/arrays/Nqueens.java
+++ src/leetcode/arrays/Nqueens.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Nqueens {
     static int count = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
