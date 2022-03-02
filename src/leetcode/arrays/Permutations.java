bbeb282c5da0aab8ef653b1691d41f3e54dca856
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-02 22:39:19 +0530

    Permutations

--- src/leetcode/arrays/Permutations.java
+++ src/leetcode/arrays/Permutations.java
@@ -1,4 +1,18 @@
 package leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Permutations {
+    static ArrayList<ArrayList<Integer>> arrayList;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        String[] str = sc.nextLine().split(" ");
+        int[] arr = new int[str.length];
+        getSol( arr);
+    }
+
+    private static void getSol(int[] arr) {
+
+    }
 }
