57bb68daa95d6746459267bfcc96fbe2084c72dd
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-02 20:38:12 +0530

    N Queens

---
+++ src/leetcode/dynamicP/Try.java
@@ -0,0 +1,44 @@
+package leetcode.dynamicP;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Try {
+    static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int[] arr = new int[str.length];
+        for ( int i = 0; i < str.length; i++){
+            arr[i] = Integer.parseInt( str[i]);
+        }
+        int n = sc.nextInt();
+        int k = 0;
+        getSol( arr, n, k, "");
+        System.out.println( arrayList);
+    }
+
+    private static void getSol(int[] arr, int target, int k, String str) {
+        if ( target == 0){
+                char[] arr2 = str.toCharArray();
+                Arrays.sort(arr2);
+                ArrayList<Integer> charList = new ArrayList<>();
+                for ( int i = 0; i < str.length(); i++){
+                    charList.add( Integer.parseInt( String.valueOf(arr2[i])));
+                }
+                if ( !arrayList.contains(arr2)){
+                    arrayList.add(charList);
+                }
+                str = "";
+                return;
+        }
+        if ( target < 0){
+            return;
+        }
+        for( int i = 0; i < arr.length; i++){
+            getSol( arr, target - arr[i], arr[i], str + String.valueOf(arr[i]));
+        }
+        return;
+     }
+}
