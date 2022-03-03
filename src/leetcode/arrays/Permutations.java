e4a23b25d907d574359ce1d55d9aa4a118bdfd94
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-03 12:24:10 +0530

    Permutations

--- src/leetcode/arrays/Permutations.java
+++ src/leetcode/arrays/Permutations.java
@@ -1,10 +1,14 @@
 package leetcode.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Permutations {
+
     static ArrayList<ArrayList<Integer>> arrayList;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
         String[] str = sc.nextLine().split(" ");
@@ -14,5 +18,23 @@ public class Permutations {
 
     private static void getSol(int[] arr) {
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> strList = new ArrayList<>();
+        String str = "";
+        for ( int i = 0; i < arr.length; i++){
+            str = str + String.valueOf( arr[i]);
+        }
+        for ( int i = 0; i < arr.length; i++){
+                String p = "";
+                int[] arr3 = new int[arr.length];
+                for ( int j = 0; j < arr.length; j++){
+                    if ( j < arr.length - 1){
+                        arr3[j + 1] = arr[ j];
+                        p = p + String.valueOf( arr3[ j + 1]);
+                    }
+                    if( j == arr.length - 1){
+                        arr3[0] = arr[arr.length];
+                    }
+                }
+        }
     }
 }
