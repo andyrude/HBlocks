f67878acd1a3e00e1eb7cc37d9a32c16a1be9d6c
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-03 18:39:42 +0530

    Histogram

--- src/leetcode/arrays/Histogram.java
+++ src/leetcode/arrays/Histogram.java
@@ -32,7 +32,7 @@ public class Histogram {
                     }
                 }
             }
-            rs[i] = j ;
+            rs[i] = j - 1 ;
         }
 
         for ( int i = arr.length - 1; i >= 0; i--){
