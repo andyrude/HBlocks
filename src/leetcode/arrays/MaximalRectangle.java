830a9c71bf9e26b3a75a6fb338ef99ce71cc86f5
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-04 20:16:03 +0530

    Maximum Rectangle

--- src/leetcode/arrays/MaximalRectangle.java
+++ src/leetcode/arrays/MaximalRectangle.java
@@ -10,8 +10,8 @@ public class MaximalRectangle {
         int m = sc.nextInt();
         int n = sc.nextInt();
         int[][] arr = new int[m][n];
-        for ( int i = 0; i < n; i++){
-            for ( int j = 0; j < m; j++){
+        for ( int i = 0; i < m; i++){
+            for ( int j = 0; j < n; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
