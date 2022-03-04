5f4a7d33867074db389fccc66c0eccdac9426fed
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-05 01:43:30 +0530

    String Permutations

--- src/leetcode/permutations/StringPermutations.java
+++ src/leetcode/permutations/StringPermutations.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class StringPermutations {
+    static ArrayList< String> arrayList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
         String str = sc.nextLine();
@@ -16,10 +18,21 @@ public class StringPermutations {
                 map.put( ch, 1);
             }
         }
-        getAllSubStrings( str);
+        getAllSubStrings( 0, str.length(), map, "");
+        System.out.println( arrayList);
     }
 
-    private static void getAllSubStrings(String str) {
-        ArrayList<String> arr = new ArrayList<>();
+    private static void getAllSubStrings(int cs, int len, HashMap< Character, Integer> map, String asf) {
+        if ( cs == len){
+            arrayList.add( asf);
+            return;
+        }
+            for ( char ch: map.keySet()){
+                if ( map.get(ch) > 0){
+                    map.put( ch, map.get(ch) - 1);
+                    getAllSubStrings( cs + 1, len, map, asf + ch);
+                    map.put( ch, map.get( ch) + 1);
+                }
+            }
     }
 }
