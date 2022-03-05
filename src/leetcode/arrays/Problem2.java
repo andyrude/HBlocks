2fa45fbaa9b1437a1a728e1cf4ba0619f27072d4
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-05 11:24:36 +0530

    Problem2

--- src/leetcode/arrays/Problem2.java
+++ src/leetcode/arrays/Problem2.java
@@ -18,18 +18,19 @@ public class Problem2 {
     private static void getNumberOfUniqueEle(int[] arr) {
         HashMap< Integer, Integer> map = new HashMap<>();
         for( int i = 0; i < arr.length; i++){
-            if ( map.containsKey( arr[i])){
-                int n = map.get( arr[i]);
-                map.remove( arr[i]);
-                map.put( arr[i], n + 1);
+            if ( !map.containsKey( arr[i])){
+               map.put(  arr[i], 1);
             }else {
-                map.put( arr[i], 1);
+                int k = map.get(arr[i]);
+                k++;
+                map.replace(arr[i], k);
             }
         }
         System.out.println( map);
         map.remove("");
         int n = 0;
         ArrayList<Integer> arrayList = new ArrayList<>( map.keySet());
+        System.out.println( arrayList);
         for (Integer i: arrayList) {
             if ( map.get(i) == 1){
                 n++;
