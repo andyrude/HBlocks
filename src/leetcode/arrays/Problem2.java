4c89d4d19021f2bcbd01649f6f2ab4cc0f665960
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-05 11:08:58 +0530

    Problem2

--- src/leetcode/arrays/Problem2.java
+++ src/leetcode/arrays/Problem2.java
@@ -19,7 +19,9 @@ public class Problem2 {
         HashMap< Integer, Integer> map = new HashMap<>();
         for( int i = 0; i < arr.length; i++){
             if ( map.containsKey( arr[i])){
-                map.put( arr[i], map.get( arr[i]) + 1);
+                int n = map.get( arr[i]);
+                map.remove( arr[i]);
+                map.put( arr[i], n + 1);
             }else {
                 map.put( arr[i], 1);
             }
