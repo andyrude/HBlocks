f1010c7d9818929fa990958bca6f1c5d3ece9bac
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-06 14:55:40 +0530

    LIS

--- src/leetcode/arrays/SortHashMapValues.java
+++ src/leetcode/arrays/SortHashMapValues.java
@@ -22,9 +22,7 @@ public class SortHashMapValues {
         System.out.println( list);
         Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
             @Override
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 return (o1.getValue()).compareTo(o2.getValue());
             }
         });
