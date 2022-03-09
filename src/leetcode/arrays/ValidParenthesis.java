063bd7f2a40773a4ea86f1849d22177d46c009df
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-09 10:14:20 +0530

    ValidParenthesis

---
+++ src/leetcode/arrays/ValidParenthesis.java
@@ -0,0 +1,31 @@
+package leetcode.arrays;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        String str = sc.nextLine();
+        System.out.println( checkValPara( str));
+    }
+
+    private static boolean checkValPara(String str) {
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        for (i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{') {
+                    stack.push(str.charAt(i));
+                } else if ((str.charAt(i) == ')' && stack.peek() == '(') || (str.charAt(i) == '}' && stack.peek() == '{') || (str.charAt(i) == ']' && stack.peek() == '[')) {
+                    stack.pop();
+                    continue;
+                }
+            }
+        if ( i == str.length() && stack.isEmpty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
